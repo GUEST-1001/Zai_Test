13fe42b9ecebbdef6a99b4981c1a133ca1cfcc28
GUEST-1001 <nawatgame@live.com>
2024-06-27 11:01:20 +0700

    Basic Random Wind

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     static public GameManager Instance;
     [SerializeField] ThrowSystem player1, player2;
+    [SerializeField] TMP_Text windText;
+
+    public float windValue;
 
     public float mainTargetSpeed;
 
@@ -13,12 +18,26 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
         player1.isTurn = true;
+        RandomWind();
     }
 
     public void FilpTurn()
     {
         player1.isTurn = !player1.isTurn;
         player2.isTurn = !player2.isTurn;
+        RandomWind();
+    }
+
+    void RandomWind()
+    {
+        windValue = Random.Range(0f, 5f);
+        switch (Random.Range(0, 2))
+        {
+            case 1:
+                windValue = -windValue;
+                break;
+        }
+        windText.text = windValue.ToString();
     }
 
 }
